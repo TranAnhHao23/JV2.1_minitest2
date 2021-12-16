1a247f5f9086e939759becd67413de1196904b83
TranAnhHao23 <Trananhhaonuce@gmail.com>
2021-12-16 15:49:06 +0700

    Add file

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,13 @@ public class Main {
         System.out.println("Menu:");
         System.out.println("1. Add new Person");
         System.out.println("2. Display all Person");
-        System.out.println("1. Add new Person");
-        System.out.println("1. Add new Person");
-        System.out.println("1. Add new Person");
-        System.out.println("1. Add new Person");
+        System.out.println("3. Find Person via ID");
+        System.out.println("4. Delete Person via ID");
+        System.out.println("5. Edit Person via ID");
+        System.out.println("6. Arrange list via averageScore");
+        System.out.println("6. Sum of Score");
+        System.out.println("0. Exit");
+        int choice;
 
     }
 }
